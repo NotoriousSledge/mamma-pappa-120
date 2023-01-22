8efebeccad1a243909ef457358d2cd78d3fd853e
Exsjabe <jakob.bergstrom@explizit.se>
2023-01-22 19:53:51 +0100

    Implemented transition using framer motion

--- src/components/fade_provider.tsx
+++ src/components/fade_provider.tsx
@@ -1,37 +1,48 @@
+import {AnimatePresence, motion} from 'framer-motion';
 import {useRouter} from 'next/router';
-import {useCallback, useEffect, useRef} from 'react';
+import {useCallback, useEffect, useState} from 'react';
+
+const variants = {
+  hidden: {opacity: 0, x: -200, y: 0},
+  enter: {opacity: 1, x: 0, y: 0},
+  exit: {opacity: 0, x: 0, y: -100},
+} as const;
 
 export const FadeProvider = ({children}: {children: React.ReactNode}) => {
-  const ref = useRef<HTMLDivElement>(null);
+  const [isVisible, setIsVisible] = useState(true);
   const {events} = useRouter();
+  const onRouteStart = useCallback(() => {
+    setIsVisible(false);
+  }, [setIsVisible]);
 
-  const fadeOut = useCallback(() => {
-    if (ref.current) {
-      ref.current.style.opacity = '0';
-    }
-  }, []);
-
-  const fadeIn = useCallback(() => {
-    if (ref.current) {
-      ref.current.style.opacity = '1';
-    }
-  }, []);
+  const onRouteExit = useCallback(() => {
+    setTimeout(() => setIsVisible(true), 500);
+  }, [setIsVisible]);
 
   useEffect(() => {
-    events.on('routeChangeStart', fadeOut);
-    events.on('routeChangeComplete', fadeIn);
-    events.on('routeChangeError', fadeIn);
-
+    events.on('routeChangeStart', onRouteStart);
+    events.on('routeChangeComplete', onRouteExit);
+    events.on('routeChangeError', onRouteExit);
     return () => {
-      events.off('routeChangeStart', fadeIn);
-      events.off('routeChangeComplete', fadeOut);
-      events.off('routeChangeError', fadeOut);
+      events.off('routeChangeStart', onRouteStart);
+      events.off('routeChangeComplete', onRouteExit);
+      events.off('routeChangeError', onRouteExit);
     };
-  }, [events, fadeIn, fadeOut]);
+  }, [events, onRouteStart, onRouteExit]);
 
   return (
-    <div ref={ref} className="duration-500 ease-in-out">
-      {children}
-    </div>
+    <AnimatePresence mode="wait" initial={false}>
+      {isVisible && (
+        <motion.main
+          variants={variants}
+          initial="hidden"
+          animate="enter"
+          exit="exit"
+          transition={{type: 'linear'}}
+        >
+          {children}
+        </motion.main>
+      )}
+    </AnimatePresence>
   );
 };
--- src/components/slide.tsx
+++ src/components/slide.tsx
@@ -16,7 +16,7 @@ export const Slide = ({index, length}: SlideProps) => {
 
   return (
     <>
-      <main className="absolute top-0 h-screen w-screen">
+      <div className="absolute top-0 h-screen w-screen">
         <Image
           src={`./slides/slide-${index}.svg`}
           fill
@@ -25,7 +25,7 @@ export const Slide = ({index, length}: SlideProps) => {
         />
         <Routing side="left" to={index - 1} hide={index === 1} />
         <Routing side="right" to={index + 1} hide={index === length} />
-      </main>
+      </div>
     </>
   );
 };
